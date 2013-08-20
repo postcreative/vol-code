7dcaa07b667eae63a70bc6f4e45448feda84a4be
Robert Busby <robert@postcreative.co.uk>
2013-08-20 10:43:43 +0100

    Header edits
    Edits to header.php and header styling

--- header.php
+++ header.php
@@ -3,7 +3,8 @@
 <!--[if lt IE 7 ]><html class="no-js ie6" lang="en"><![endif]--> 
 <!--[if IE 7 ]><html class="no-js ie7" lang="en"><![endif]--> 
 <!--[if IE 8 ]><html class="no-js ie8" lang="en"><![endif]--> 
-<!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html class="no-js" lang="en"><!--<![endif]-->
+<!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!-->
+<html class="no-js" lang="en"><!--<![endif]-->
 	<head>
 		<title><?php bloginfo( 'name' ); ?><?php wp_title( '|' ); ?></title>
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
@@ -12,36 +13,32 @@
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon.ico"/>
 		<?php wp_head(); ?>
-		<!--[if IE]>
-    		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-		<![endif]-->
+		<!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 		<link href='http://fonts.googleapis.com/css?family=Quicksand:400,700' rel='stylesheet' type='text/css'>
 	</head>
 <body <?php body_class(); ?>>
 <div class="container">
-<header>
-<div id="brand" class="clearfix">
-	<div class="pull-left">
-		<h1><a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a></h1>
-	</div>
-</div>
-<div class="navbar">
-  <div class="navbar-inner">
-		<nav role="navigation">
-			<?php 
-			    wp_nav_menu( array(
-			        'menu'       => 'header-menu',
-			        'depth'      => 2,
-			        'container'  => false,
-			        'menu_class' => 'nav',
-			        'fallback_cb' => 'wp_page_menu',
-			        //Process nav menu using our custom nav walker
-			        'walker' => new wp_bootstrap_navwalker())
-			    );
-			?>
-		</nav><!-- /nav -->
- 	</div>
-</div>
-<div id="banner">
-</div>
-</header>
+	<header>
+		<div id="brand" class="clearfix">
+			<h1 class"pull-left"><a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a></h1>
+		</div>
+		<div class="navbar">
+		  <div class="navbar-inner">
+				<nav role="navigation">
+					<?php 
+					    wp_nav_menu( array(
+					        'menu'       => 'header-menu',
+					        'depth'      => 2,
+					        'container'  => false,
+					        'menu_class' => 'nav',
+					        'fallback_cb' => 'wp_page_menu',
+					        //Process nav menu using our custom nav walker
+					        'walker' => new wp_bootstrap_navwalker())
+					    );
+					?>
+				</nav><!-- /nav -->
+		 	</div>
+		</div>
+		<div id="banner">
+		</div>
+	</header>
